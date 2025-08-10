36f7cd781ca4417bff985d48c9dd55aa12987d18
aer675 <s10273247@connect.np.edu.sg>
2025-08-10 16:08:27 +0800

    a key

--- Sundrop_Cave_format.py
+++ Sundrop_Cave_format.py
@@ -314,7 +314,33 @@ def handle_mine_menu():
                     print("You moved north.")  
             else:
                 print("You can't move north, you are at the top of the mine.")
+                
         elif choice == 'a':
+            if player['x'] > 0:
+                player['x'] -= 1
+                player['steps'] += 1
+                player['turns'] -= 1
+                if game_map[player['y']][player['x']] == 'C':
+                    pieces = randint(1, 3)  # Random number of pieces of copper ore
+                    player['copper'] += pieces
+                    print(f"You mined {pieces} pieces of copper ore!")  
+                elif game_map[player['y']][player['x']] == 'S':
+                    pieces = randint(1, 2)  # Random number of pieces of silver ore
+                    player['silver'] += pieces
+                    print(f"You mined {pieces} pieces of silver ore!")
+                elif game_map[player['y']][player['x']] == 'G':
+                    pieces = randint(1, 1)  # Random number of pieces of gold ore
+                    player['gold'] += pieces
+                    print(f"You mined {pieces} pieces of gold ore!")
+                elif game_map[player['y']][player['x']] == 'T':
+                    print("You stepped on the teleport square! You are being teleported to Sundrop Town!")
+                    player['turns'] = TURNS_PER_DAY # Reset turns for the next day
+                    player['day'] += 1
+                    return 'town'
+                else:
+                    print("You moved west.")    
+            else:
+                print("You can't move west, you are at the left edge of the mine.") 
         
         elif choice == 's':
         
